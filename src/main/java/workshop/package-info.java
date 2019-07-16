33832818c1eb712b9b8d691e62b342d182cf6723
WojTechM <wojciech.m1997@gmail.com>
2019-07-16 13:26:01 +0200

    Dodanie package-info
    Ostrzeżenia i informacje dla ludzi chcących ukończyć warsztaty

---
+++ src/main/java/workshop/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * <h1>Zanim zaczniesz pracować z tym pakietem!</h1>
+ * Po pierwsze - przepraszam.
+ * Nie chciało mi się wszędzie pisać "byłeś/łaś", lub "robiłeś/łaś", więc
+ * zwracam się do czytającego jakby był mężczyzną. Wszystkie panie urażone tym faktem
+ * przepraszam i zapraszam do postawienia <i>Pull Requesta</i> z poprawką ;)
+ * <p><br/>
+ * Po drugie - dokumentacja w klasach (podobnie jak tutaj) zawiera
+ * <i>z</i><b>n</b><em>a</em><sub>c</sub><sup>z</sup><small>n</small><ins>i</ins><sub>k</sub><sup>i</sup> HTML.
+ * By dobrze je wyświetlić kliknij ['Ctrl' + 'q'] dwukrotnie.
+ * <p><br/>
+ * Będziesz musiał stworzyć sobie również pomocniczy projekt, w którym użyjesz Java1.4.
+ * Możesz go przygotować od razu ;)
+ * <p><br/>
+ * Zadania robić po kolei - są ponumerowane. Nie zgubisz się ;)
+ *
+ * @author Wojciech Makiela
+ */
+package workshop;
